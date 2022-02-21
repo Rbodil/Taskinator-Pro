df0a7dd52599f2546aede3004b5cccbf1061c1f7
Reagan Bodily <reaganbodily@gmail.com>
2022-02-21 15:50:47 -0700

    working on drag/drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,69 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// compare with code snippet
+$(".list-group").sortable({
+  conectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+    console.log("activate", this);
+  },
+  deactivate: function(event){
+    console.log("deactivate", this);
+  },
+  over: function(event){
+    console.log("over", event.target);
+  },
+  out: function(event){
+    console.log("out",event.target);
+  },
+  update: function(event, ui){
+    var tempArr = [];
+    
+    $(this).children().each(function() {
+      
+      var text = $(this)
+        .find('p')
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find('span')
+        .text()
+        .trim();
+
+
+        tempArr.push({
+          text: text,
+          date: date
+        });
+      });
+    console.log(tempArr);
+
+    var arrName = $(this)
+      .attr('id')
+      .replace("list-", "");
+
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$('#trash').droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui){
+    ui.draggable.remove();
+  },
+  over: function(event, ui){
+    console.log("over");
+  },
+  out: function(event, ui){
+    console.log("out")
+  }
+});
 
 
 
